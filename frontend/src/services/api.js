76dfee2c1d9a5708b9041c871068b17e6440b8f7
Aman Tiwari <tiwari95aman@gmail.com>
2026-01-14 12:31:39 +0530

    new fix

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,8 +1,22 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
+  baseURL: import.meta.env.VITE_API_URL,
   withCredentials: true,
+});
+
+
+api.interceptors.request.use((config) => {
+  const userInfo = localStorage.getItem("userInfo") 
+    ? JSON.parse(localStorage.getItem("userInfo")) 
+    : null;
+
+  if (userInfo && userInfo.token) {
+    config.headers.Authorization = `Bearer ${userInfo.token}`;
+  }
+  return config;
+}, (error) => {
+  return Promise.reject(error);
 });
 
 export default api;
